07c9563ae997d8443c737fce6c580ebd9c45b1cb
Alex Bollas <azbollas@yahoo.com>
2019-01-06 21:57:00 -0500

    added new controller in API to return all submitted times

---
+++ awhcode-clockwork-repo-a3b620f80baa/Clockwork.API/Controllers/SubmittedTimesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SubmittedTimesController : Controller
+    {
+        // GET api/submittedtimes
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            //var returnVal = new CurrentTimeQuery
+            //{
+            //    UTCTime = utcTime,
+            //    ClientIp = ip,
+            //    Time = serverTime
+            //};
+            List<CurrentTimeQuery> times = new List<CurrentTimeQuery>();
+            using (var db = new ClockworkContext())
+            {
+                
+                times = db.CurrentTimeQueries.ToList();
+                
+                Console.WriteLine();
+                foreach (var CurrentTimeQuery in times)
+                {
+                    Console.WriteLine(" - {0}", CurrentTimeQuery.UTCTime);
+                }
+            }
+
+            return Ok(times);
+        }
+    }
+}
